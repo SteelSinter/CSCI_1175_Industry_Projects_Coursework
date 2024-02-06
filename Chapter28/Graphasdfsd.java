65eca86102327ad7baefded5553dc406a52a8316
James <145038163+SteelSinter@users.noreply.github.com>
2024-02-06 10:47:27 -0700

    Add files via upload

---
+++ Chapter28/Graphasdfsd.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class Graphasdfsd extends Application {
+	
+	@Override
+	public void start(Stage mainStage) {
+		Graph graph = new UnweightedGraph();
+	}
+
+	public static void main(String[] args) {
+		Application.launch(args);
+
+	}
+
+}
